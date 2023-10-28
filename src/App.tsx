3f5e30c34581b85e0c23ad9f51cea590494a3d5d
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-10-27 23:54:28 -0300

    initial config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,45 @@
 function App() {
   return (
     <>
-      aaaaaaa
+      <header>
+          <h1 className="">
+              Hello world!
+          </h1>
+      </header>
+
+        <section>
+            <input />
+            <button type='submit'>
+                Criar
+            </button>
+        </section>
+
+        <section>
+            <div>
+                <span>
+                    Tarefas criadas
+                    <strong>5</strong>
+                </span>
+
+                <span>
+                    Concluídas
+                    <strong>2 de 5</strong>
+                </span>
+            </div>
+            <div>
+                <div>
+                    Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+                </div>
+
+                <div>
+                    Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+                </div>
+
+                <div>
+                    Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+                </div>
+            </div>
+        </section>
     </>
   )
 }
